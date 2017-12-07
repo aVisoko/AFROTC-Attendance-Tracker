71d00c934a3208799a289f90bbf17c4ace619e9c
mister-mellow <33880237+mister-mellow@users.noreply.github.com>
2017-12-07 17:54:02 -0600

    update

--- PHP_Files/login.php
+++ PHP_Files/login.php
@@ -2,19 +2,14 @@
 
 include 'config.php';
 
-	// Here we are using sessions to propagate the login
-	// http://us3.php.net/manual/en/intro.session.php
-
-    // HTTPS redirect
     if ($_SERVER['HTTPS'] !== 'on') {
 		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 		header("Location: $redirectURL");
 		exit;
 	}
 	
-	// http://us3.php.net/manual/en/function.session-start.php
+
 	if(!session_start()) {
-		// If the session couldn't start, present an error
 		header("Location: error.php");
 		exit;
 	}
@@ -40,24 +35,7 @@ include 'config.php';
 	function handle_login() {
 		$username = empty($_POST['username']) ? '' : $_POST['username'];
 		$password = empty($_POST['password']) ? '' : $_POST['password'];
-	
-        /*
-		if ($username == "test" && $password == "pass") {
-			// Instead of setting a cookie, we'll set a key/value pair in $_SESSION
-			$_SESSION['loggedin'] = $username;
-			header("Location: page1.php");
-			exit;
-		}
-        */
-        
-        // We added the test user to our users table
-        // INSERT INTO users (username, password, addDate, changeDate) VALUES ('test', 'pass', NOW(), NOW());
-        
-        // Require the credentials
-        
 
-        
-        // Connect to the database
         $mysqli = new mysqli(host, testuse, testpass, dbname);
         
         // Check for errors
@@ -67,35 +45,28 @@ include 'config.php';
             exit;
         }
         
-        // http://php.net/manual/en/mysqli.real-escape-string.php
+ 
         $username = $mysqli->real_escape_string($username);
         $password = $mysqli->real_escape_string($password);
         
-        //more secure password storing for website
+
        
         
-        // Build query
+  
 		$query = "SELECT usersinfo.username, usersinfo.password FROM usersinfo WHERE username = '$username' AND password = '$password'";
 		
-        // Sometimes it's nice to print the query. That way you know what SQL you're working with.
-        //print $query;
-        //exit;
+  
         
-		// Run the query
+	
 		$mysqliResult = $mysqli->query($query);
 		
-        // If there was a result...
+    
         if ($mysqliResult) {
-            // How many records were returned?
+         
             $match = $mysqliResult->num_rows;
-
-            // Close the results
             $mysqliResult->close();
-            // Close the DB connection
             $mysqli->close();
 
-
-            // If there was a match, login
   		    if ($match == 1) {
                 $_SESSION['loggedin'] = $username;
                 header("Location: page1.php");
@@ -107,7 +78,6 @@ include 'config.php';
                 exit;
             }
         }
-        // Else, there was no result
         else {
           $error = 'Login Error: Please contact the system administrator.';
           require "login_form.php";
